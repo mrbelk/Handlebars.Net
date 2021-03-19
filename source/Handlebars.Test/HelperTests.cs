5799a51252daff4cc204c85e8d02684de180b710
Matthew Belk <mbelk@bizspeed.com>
2021-03-19 10:36:23 -0400

    Update HelperTests.cs
    Add test for iteration inside block helper

--- source/Handlebars.Test/HelperTests.cs
+++ source/Handlebars.Test/HelperTests.cs
@@ -745,6 +745,126 @@ namespace HandlebarsDotNet.Test
         }
 
         [Fact]
+        public void BlockHelperWithEnumeration()
+        {
+            Handlebars.RegisterHelper ("ifCond", (writer, options, context, args) => {
+                if (args.Length != 3) {
+                    writer.Write ("ifCond:Wrong number of arguments");
+                    return;
+                }
+                if (args [0] == null || args [0].GetType ().Name == "UndefinedBindingResult") {
+                    writer.Write ("ifCond:args[0] undefined");
+                    return;
+                }
+                if (args [1] == null || args [1].GetType ().Name == "UndefinedBindingResult") {
+                    writer.Write ("ifCond:args[1] undefined");
+                    return;
+                }
+                if (args [2] == null || args [2].GetType ().Name == "UndefinedBindingResult") {
+                    writer.Write ("ifCond:args[2] undefined");
+                    return;
+                }
+                if (args [0].GetType ().Name == "String" || args [0].GetType ().Name == "JValue") {
+                    var val1 = args [0].ToString ();
+                    var val2 = args [2].ToString ();
+
+                    switch (args [1].ToString ()) {
+                    case ">":
+                        if (val1.Length > val2.Length) {
+                            options.Template (writer, context);
+                        } else {
+                            options.Inverse (writer, context);
+                        }
+                        break;
+                    case "=":
+                    case "==":
+                        if (val1 == val2) {
+                            options.Template (writer, context);
+                        } else {
+                            options.Inverse (writer, context);
+                        }
+                        break;
+                    case "%=": // case-insensitive comparison
+                        if (val1.Equals (val2, StringComparison.OrdinalIgnoreCase)) {
+                            options.Template (writer, context);
+                        } else {
+                            options.Inverse (writer, context);
+                        }
+                        break;
+                    case "<":
+                        if (val1.Length < val2.Length) {
+                            options.Template (writer, context);
+                        } else {
+                            options.Inverse (writer, context);
+                        }
+                        break;
+                    case "!=":
+                    case "<>":
+                        if (val1 != val2) {
+                            options.Template (writer, context);
+                        } else {
+                            options.Inverse (writer, context);
+                        }
+                        break;
+                    case "%!=":
+                    case "%<>":
+                        if (!val1.Equals (val2, StringComparison.OrdinalIgnoreCase)) {
+                            options.Template (writer, context);
+                        } else {
+                            options.Inverse (writer, context);
+                        }
+                        break;
+                    }
+                } else {
+                    var val1 = float.Parse (args [0].ToString ());
+                    var val2 = float.Parse (args [2].ToString ());
+
+                    switch (args [1].ToString ()) {
+                    case ">":
+                        if (val1 > val2) {
+                            options.Template (writer, context);
+                        } else {
+                            options.Inverse (writer, context);
+                        }
+                        break;
+                    case "=":
+                    case "==":
+                        if (val1 == val2) {
+                            options.Template (writer, context);
+                        } else {
+                            options.Inverse (writer, context);
+                        }
+                        break;
+                    case "<":
+                        if (val1 < val2) {
+                            options.Template (writer, context);
+                        } else {
+                            options.Inverse (writer, context);
+                        }
+                        break;
+                    case "!=":
+                    case "<>":
+                        if (val1 != val2) {
+                            options.Template (writer, context);
+                        } else {
+                            options.Inverse (writer, context);
+                        }
+                        break;
+                    }
+                }
+            });
+
+            var template = Handlebars.Compile (@"{{#ifCond arg1.Count '>' 0}}{{#each arg1}}{{@this}},{{/each}}{{else}}nothing to enumerate{{/ifCond}}");
+            var data = new { arg1 = new List<string> { "one", "two", "three" } };
+            var result = template (data);
+            Assert.Equal ("one,two,three,", result);
+
+            data = new { arg1 = new List<string> () };
+            result = template (data);
+            Assert.Equal ("nothing to enumerate", result);
+        }
+
+        [Fact]
         public void HelperWithNumericArguments()
         {
             Handlebars.RegisterHelper("myHelper", (writer, context, args) => {
